8eb186c9b7fd1976dfba6326b12f21f8d63c0051
idlewith <newellzhou@163.com>
2023-11-07 01:56:37 +0800

    fix: run from cli

--- src/lan_file/main.py
+++ src/lan_file/main.py
@@ -36,10 +36,14 @@ def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
-if __name__ == "__main__":
+def main():
     if not os.path.exists(app.config["UPLOAD_FOLDER"]):
         os.makedirs(app.config["UPLOAD_FOLDER"])
 
     port = 5555
     print(f"Running on http://{net.get_local_ip()}:{port}")
     run_simple("0.0.0.0", port, app)
+
+
+if __name__ == "__main__":
+    main()
